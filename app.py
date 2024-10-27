3549ede64d682111742c6c4789d92eb06619c837
Saad Latheef <142167756+saad-latheef@users.noreply.github.com>
2024-10-27 11:27:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,12 @@ import random
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
-# Importing libraries
-import imaplib, email
-gpay=[]
-user = 'refoteam0@gmail.com'
-password = 'phtn hgeb slsn wxdu'
-imap_url = 'imap.gmail.com'
-con = imaplib.IMAP4_SSL(imap_url)  
-con.login(user, password)
-# Function to get email content part i.e its body part
-def get_body(msg):
-    if msg.is_multipart():
-        return get_body(msg.get_payload(0))
-    else:
-        return msg.get_payload(None, True)
+
+
+
+
  
-# Function to search for a key value pair 
-def search(key, value, con): 
-    result, data = con.search(None, key, '"{}"'.format(value))
-    return data
- 
-# Function to get the list of emails under this label
-def get_emails(result_bytes):
-    msgs = [] # all the email data are pushed inside an array
-    for num in result_bytes[0].split():
-        typ, data = con.fetch(num, '(RFC822)')
-        msgs.append(data)
- 
-    return msgs
+
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
@@ -147,43 +125,8 @@ def dashboard():
 
 @app.route('/expenses', methods=['GET', 'POST'])
 def expenses():
-    global gpay
     global expenses_data
-    if request.method == 'POST':
- 
-        con.select('Inbox')
-        msgs = get_emails(search('FROM', 'no-reply@sms-forwarder.com', con))
-        list=[]
-        c=0
-        fc=-1
-        while fc>=-20:
-            msgg=msgs[fc][0][1]
-            l= msgg.split()
-            for i in l:
-                f=i.decode('utf-8')
-                if f[0]=='(' and f[1].isdigit():
-                    c=0
-                if c==1:
-                    list.append(f)
-                if f=='br/>Message:':
-                    c=1
-            if list==[]:
-                break
-            else:
-                amounts = list[1]
-                if amounts[0].isdigit():
-                    categorys='gpay'
-                    amnt=float(amounts)
-                    if {'category': categorys, 'amount': amnt} not in gpay:
-                        expenses_data.append({'category': categorys, 'amount': amnt})
-                        gpay.append({'category': categorys, 'amount': amnt})
-                l=[]
-                list=[]
-                fc-=1
-                print(expenses_data)
-                print(gpay)
-
-
+    if request.method == 'POST:
         category = request.form.get('category')
         amount = float(request.form.get('amount', 0))
         if category and amount:
